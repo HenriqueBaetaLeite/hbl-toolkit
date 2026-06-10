ba8146a9377c4964de69b7e93de8d4807310ddb9
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2026-06-10 13:15:17 -0300

    fix TimerRunner

--- src/components/timer/TimerRunner.tsx
+++ src/components/timer/TimerRunner.tsx
@@ -1,91 +1,43 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useTimerEngine } from "@/core/timer-engine/useTimerEngine";
 
-import { useTimerEngine } from '@/core/timer-engine/useTimerEngine';
+import { formatTime } from "@/utils/formatTime";
 
-import { formatTime } from '@/utils/formatTime';
+import { TimerSession } from "@/core/timer-engine/types";
 
-import { TimerSession } from '@/core/timer-engine/types';
-
-import { AudioManager } from '@/core/audio/AudioManager';
-
-import { TimerDisplay } from './TimerDisplay';
-import { TimerControls } from './TimerControls';
-import { ProgressBar } from './ProgressBar';
+import { TimerDisplay } from "./TimerDisplay";
+import { TimerControls } from "./TimerControls";
+import { ProgressBar } from "./ProgressBar";
 
 interface TimerRunnerProps {
   session: TimerSession;
 }
 
-
-export function TimerRunner({
-  session,
-}: TimerRunnerProps) {
+export function TimerRunner({ session }: TimerRunnerProps) {
   const {
-    engine,
     currentStep,
     remainingTime,
     progress,
     start,
     pause,
-    resume,
+    // resume,
     reset,
-    status,
   } = useTimerEngine(session);
-
-  console.log('TimerRunner renderizou');
-
-  useEffect(() => {
-
-  console.log('ENGINE', engine);
-
-  if (!engine) {
-    console.log('ENGINE NULO');
-    return;
-  }
-
-  console.log('VOU REGISTRAR EVENTOS');
-
-  const unsubscribe =
-    engine.subscribeEvents((event) => {
-
-      console.log('EVENTO RECEBIDO', event);
-
-    });
-
-  console.log('EVENTOS REGISTRADOS');
-
-  return unsubscribe;
-
-}, [engine]);
-
-//   useEffect(() => {
-//   if (!engine) return;
-
-//   console.log('Registrando eventos');
-
-//   const unsubscribe = engine.subscribeEvents((event) => {
-//     console.log('EVENTO RECEBIDO', event);
-//   });
-
-//   return unsubscribe;
-// }, [engine]);
 
   return (
     <div className="flex flex-col gap-8 w-full">
       <TimerDisplay
-        title={currentStep?.name ?? ''}
+        title={currentStep?.name ?? ""}
         time={formatTime(remainingTime)}
       />
 
       <ProgressBar value={progress} />
 
       <TimerControls
-        status={status}
         onStart={start}
         onPause={pause}
-        onResume={resume}
+        // onResume={resume}
         onReset={reset}
       />
     </div>
